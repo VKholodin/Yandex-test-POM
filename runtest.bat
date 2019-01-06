14a4c47e45f37fbff0b59ca1cf72ca50ef7179a6
VKholodin <vitalik-5k@yandex.ru>
2019-01-06 23:18:00 +0300

    Update runtest.bat

--- runtest.bat
+++ runtest.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 
-vstest.console.exe "D:\My_GIT\YandexSimpleTest\YandexSimpleTest\bin\Debug\YandexSimpleTest.dll" /Tests:Verify_Yandex_Link_As_Fisrt,  /logger:trx 
+vstest.console.exe "C:\Your_location\YandexSimpleTest\YandexSimpleTest\bin\Debug\YandexSimpleTest.dll" /Tests:Verify_Yandex_Link_As_Fisrt,  /logger:trx 
